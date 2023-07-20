9815bd2b53d4d2bab4fefdaa4f7f8ed528688c42
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-07-20 09:58:19 +0530

    class 17 notes

--- JavaScript/class 17/machineCoding/starComponent/script.js
+++ JavaScript/class 17/machineCoding/starComponent/script.js
@@ -1,6 +1,7 @@
 let star = document.querySelectorAll(".star");
 
 let container = document.getElementById("container");
+let count = document.getElementById("count");
 let filled = 0;
 container.addEventListener("click", (e) => {
   let currentClicked = e.target.dataset.index;
@@ -10,5 +11,6 @@ container.addEventListener("click", (e) => {
   for (let i = 0; i < currentClicked; i++) {
     star[i].classList.add("star-colored");
   }
+  count.innerText = currentClicked;
   filled = currentClicked;
 });
