b54127affd0f791ba7fb8a0f17b53c7ae992df87
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-02 08:17:35 +0530

    navbar changes

---
+++ Projects/imdb_project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './components/Home/NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/imdb_project/src/components/Home/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../MovieLogo.png'
+const NavBar = () => {
+  return (
+    <div className='flex border space-x-8 items-center pl-3 py-4'>
+        <img src={Logo} className='w-[60px]'/>
+        <h3 className='text-blue-400'>Movies</h3>
+        <h3 className='text-blue-400'>WatchList</h3>
+    </div>
+  )
+}
+
+export default NavBar
