21fb9c07650e174e0a27ee9cae26b3331b046f48
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-02 08:39:03 +0530

    banner related code

--- Projects/imdb_project/src/App.js
+++ Projects/imdb_project/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/Home/NavBar';
+import Banner from './components/Home/Banner';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Banner />
     </div>
   );
 }
