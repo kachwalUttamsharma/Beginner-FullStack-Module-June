68b7dd8fcc160759e4579d69d29f70581b84c14d
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-07 08:43:29 +0530

    table design

--- Projects/imdb_project/src/components/Home/Movies.js
+++ Projects/imdb_project/src/components/Home/Movies.js
@@ -6,55 +6,61 @@ const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [counter, setCounter] = useState(1);
   const [watchList, setWatchList] = useState([]);
-  const [hovered, setHovered] = useState('');
+  const [hovered, setHovered] = useState("");
 
   useEffect(() => {
-  //  GetTrendingMovies(counter).then((result) => {
-  //   setMovies(result);
-  //  })
-   async function fetchMyMovies() {
-    const result = await GetTrendingMovies(counter)
-    console.log(result);
-    setMovies(result);
-  }
-  fetchMyMovies()
+    //  GetTrendingMovies(counter).then((result) => {
+    //   setMovies(result);
+    //  })
+    async function fetchMyMovies() {
+      const result = await GetTrendingMovies(counter);
+      console.log(result);
+      setMovies(result);
+    }
+    fetchMyMovies();
   }, [counter]);
 
   const onNext = () => {
-    setCounter(counter+1);
-  }
+    setCounter(counter + 1);
+  };
 
   const onPrev = () => {
-    if(counter == 1) {
+    if (counter == 1) {
       setCounter(1);
     } else {
-      setCounter(counter-1);
+      setCounter(counter - 1);
     }
-  }
+  };
 
   const addMovieToWatchList = (id) => {
-      const newWatchList = [...watchList, id];
-      setWatchList(newWatchList);
-      // setWatchList((prevState) => [...prevState, id]);
-      //console.log("adding ", newWatchList);
-  }
+    // const newWatchList = [...watchList, id];
+    // setWatchList(newWatchList);
+    setWatchList((prevState) => [...prevState, id]);
+    //console.log("adding ", newWatchList);
+  };
 
   const removeMovieFromWatchList = (id) => {
-      // [1,2,3], (2) -> [1,3]
-      const filteredWatchList = watchList.filter((movieId) => {
-          return movieId !== id
+    // [1,2,3], (2) -> [1,3]
+    //   const filteredWatchList = watchList.filter((movieId) => {
+    //     return movieId !== id
+    // });
+    // setWatchList(filteredWatchList);
+    setWatchList((prevState) => {
+      const filteredWatchList = prevState.filter((movieId) => {
+        return movieId !== id;
       });
-      setWatchList(filteredWatchList);
-      //console.log("removing ", filteredWatchList);
-  }
+      return filteredWatchList;
+    });
+    //console.log("removing ", filteredWatchList);
+  };
 
   const showButton = (id) => {
-      setHovered(id);
-  }
+    setHovered(id);
+  };
 
   const hideButton = () => {
-      setHovered('')
-  }
+    setHovered("");
+  };
 
   return (
     <div>
@@ -68,18 +74,24 @@ const Movies = () => {
               style={{
                 backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`,
               }}
-              onMouseOver={() => {showButton(movie.id)}}
+              onMouseOver={() => {
+                showButton(movie.id);
+              }}
               onMouseOut={hideButton}
             >
-              <div className="absolute top-2 right-2 bg-gray-900 rounded-2xl p-2 text-2xl" 
-              style={{display: hovered === movie.id ? 'block' : 'none'}}>
-                {
-                  watchList.includes(movie.id) === true ? (
-                    <button onClick={() => removeMovieFromWatchList(movie.id)}>❌</button>
-                  ) : (
-                    <button onClick={() => addMovieToWatchList(movie.id)}>😍</button>
-                  )
-                }
+              <div
+                className="absolute top-2 right-2 bg-gray-900 rounded-2xl p-2 text-2xl"
+                style={{ display: hovered === movie.id ? "block" : "none" }}
+              >
+                {watchList.includes(movie.id) === true ? (
+                  <button onClick={() => removeMovieFromWatchList(movie.id)}>
+                    ❌
+                  </button>
+                ) : (
+                  <button onClick={() => addMovieToWatchList(movie.id)}>
+                    😍
+                  </button>
+                )}
               </div>
               <div className="text-white text-center font-bold w-full bg-gray-900 bg-opacity-60">
                 {movie.title}
@@ -88,7 +100,7 @@ const Movies = () => {
           );
         })}
       </div>
-      <Pagination onNext={onNext} onPrev={onPrev} counter={counter}/>
+      <Pagination onNext={onNext} onPrev={onPrev} counter={counter} />
     </div>
   );
 };
