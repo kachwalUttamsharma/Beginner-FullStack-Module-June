468c4c346795f99462e6ec774518232b3eb79bb7
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-09 08:35:14 +0530

    search filter implementation

--- Projects/imdb_project/src/components/WatchList/Table/Table.jsx
+++ Projects/imdb_project/src/components/WatchList/Table/Table.jsx
@@ -6,14 +6,23 @@ import { genreids } from "../Mock/watchListData";
 const Table = ({ favourites = [] }) => {
   const [genre, setGenre] = useState([]);
   const [currGenre, setCurrGenre] = useState("All Genre");
+  const [searchStr, setSearchStr ] = useState("");
   useEffect(() => {
     const temp = favourites.map((movie) => genreids[movie.genre_ids[0]]);
     setGenre(["All Genre", ...temp]);
-  },[favourites]);
+  }, [favourites]);
 
   let filteredMovies = [];
-  filteredMovies = currGenre === "All Genre" ? favourites : favourites.filter((movie) => 
-  genreids[movie.genre_ids[0]] == currGenre);
+  // applying genre filter
+  filteredMovies =
+    currGenre === "All Genre"
+      ? favourites
+      : favourites.filter((movie) => genreids[movie.genre_ids[0]] == currGenre);
+
+  // applying search filter
+  filteredMovies = favourites.filter((movie) => {
+    return movie.title.toLowerCase().includes(searchStr.toLowerCase());
+  })    
 
   return (
     <>
@@ -21,13 +30,30 @@ const Table = ({ favourites = [] }) => {
         {genre &&
           genre.length > 0 &&
           genre.map((eachGenre) => {
-            return <div  className={
-              currGenre == eachGenre
-                ? "m-2 text-lg p-1 px-2 bg-blue-400 text-white rounded-xl font-bold"
-                : "m-2 text-lg p-1 px-2 bg-gray-400 hover:bg-blue-400 text-white rounded-xl font-bold"
-            }
-            onClick={() => {setCurrGenre(eachGenre)}}>{eachGenre}</div>;
+            return (
+              <div
+                className={
+                  currGenre == eachGenre
+                    ? "m-2 text-lg p-1 px-2 bg-blue-400 text-white rounded-xl font-bold"
+                    : "m-2 text-lg p-1 px-2 bg-gray-400 hover:bg-blue-400 text-white rounded-xl font-bold"
+                }
+                onClick={() => {
+                  setCurrGenre(eachGenre);
+                }}
+              >
+                {eachGenre}
+              </div>
+            );
           })}
+      </div>
+      <div className="text-center">
+        <input
+          type="text"
+          className="border bg-gray-200 border-4 text-center p-1 m-2"
+          placeholder="search for the Movie"
+          value={searchStr}
+          onChange={(e) => setSearchStr(e.target.value)}
+        />
       </div>
       <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
         <TableHeader />
