e6a8666cbeb959b016ccb1b4fb1dee665e670175
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-16 08:46:41 +0530

    thunk using createasyncthunk

--- Projects/shopping_cart/src/Store/productSlice.js
+++ Projects/shopping_cart/src/Store/productSlice.js
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from 'axios';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const STATUSES = {
   SUCCESS: "success",
@@ -12,28 +12,48 @@ const productSlice = createSlice({
     data: [],
     status: STATUSES.LOADING,
   },
-  reducers: {
-    setProducts(state, action) {
-      state.data = action.payload;
-    },
-    setStatus(state, action) {
-      state.status = action.payload;
-    },
-  },
+//   reducers: {
+//     setProducts(state, action) {
+//       state.data = action.payload;
+//     },
+//     setStatus(state, action) {
+//       state.status = action.payload;
+//     },
+//   },
+    extraReducers: (builder) => {
+      builder.addCase(fetchProducts.pending, (state) => {
+          state.status = STATUSES.LOADING;
+      }).addCase(fetchProducts.fulfilled, (state, action) => {
+          state.data = action.payload;
+          state.status = STATUSES.SUCCESS;
+      }).addCase(fetchProducts.rejected, (state) => {
+          state.status = STATUSES.ERROR;
+      })
+    }
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
-export const fetchProducts = () => {
-  return async function fetchProductsThunk(dispatch) {
-    dispatch(setStatus(STATUSES.LOADING));
-    try {
-      const result = await axios.get("https://fakestoreapi.com/products");
-      dispatch(setProducts(result.data));
-      dispatch(setStatus(STATUSES.SUCCESS));
-    } catch (error) {
-      dispatch(setStatus(STATUSES.ERROR));
-    }
-  };
-};
+// export const fetchProducts = () => {
+//   return async function fetchProductsThunk(dispatch, getState) {
+//     const { products } = getState();
+//     if (products.status === STATUSES.SUCCESS && products.data.length > 0) {
+//       return;
+//     } else {
+//       dispatch(setStatus(STATUSES.LOADING));
+//       try {
+//         const result = await axios.get("https://fakestoreapi.com/products");
+//         dispatch(setProducts(result.data));
+//         dispatch(setStatus(STATUSES.SUCCESS));
+//       } catch (error) {
+//         dispatch(setStatus(STATUSES.ERROR));
+//       }
+//     }
+//   };
+// };
+
+export const fetchProducts = createAsyncThunk('products', async () => {
+    const result = await axios.get("https://fakestoreapi.com/products");
+    return result.data;
+})
