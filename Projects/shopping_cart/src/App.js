a32c4fe429637735b8c9cd2779ec7da2c9726195
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-20 21:26:13 +0530

    persist store impl

--- Projects/shopping_cart/src/App.js
+++ Projects/shopping_cart/src/App.js
@@ -3,10 +3,10 @@ import Cart from "./Components/Cart";
 import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Provider} from 'react-redux';
-import Store from './Store/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
+import { Provider } from "react-redux";
+import Store from "./Store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const persistor = persistStore(Store);
 
@@ -14,15 +14,15 @@ function App() {
   return (
     <div className="App">
       <Provider store={Store}>
-      <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-        </Routes>
-      </BrowserRouter>
-      </PersistGate>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/cart" element={<Cart />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </div>
   );
