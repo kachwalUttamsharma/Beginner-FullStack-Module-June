a7f101aafac633e6730efc1b453f4190804f186b
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-09-26 08:21:31 +0530

    imdb lazy loaded

--- Projects/imdb_project/src/App.js
+++ Projects/imdb_project/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import React, { Suspense, lazy } from 'react';
 import NavBar from "./components/Home/NavBar";
 import Banner from "./components/Home/Banner";
 import Movies from "./components/Home/Movies";
-import WatchList from "./components/WatchList/WatchList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const WatchList = lazy(() => import('./components/WatchList/WatchList'));
 function App() {
   return (
     <div>
@@ -18,7 +19,7 @@ function App() {
             </>
           }> 
           </Route>
-          <Route path="/watchList" element={<WatchList />}></Route>
+          <Route path="/watchList" element={<Suspense fallback={<div>Loading...</div>}><WatchList /></Suspense>}></Route>
         </Routes>
 
       </BrowserRouter>
