0bcac845845e44ac16092293b5b388c1ac16682a
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-10-17 09:21:42 +0530

    movies list and movies form

--- Full-Stack-Project/ScalerShow/client/src/Pages/Admin/index.jsx
+++ Full-Stack-Project/ScalerShow/client/src/Pages/Admin/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Tabs } from "antd";
+import PageTitle from '../../Components/PageTitle';
+import MoviesList from './MoviesList';
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div>
+      <PageTitle title="Admin"/>
+
+      <Tabs defaultActiveKey='1'>
+          <Tabs.TabPane key="1" tab="Movies">
+            <MoviesList />
+          </Tabs.TabPane>
+          <Tabs.TabPane key="2" tab="Theatres">{"Theatre"}</Tabs.TabPane>
+      </Tabs>
+    </div>
   )
 }
 
