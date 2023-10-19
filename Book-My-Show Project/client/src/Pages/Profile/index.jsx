4ae86fe7329040c029646922380275415f5ba6a9
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2023-10-19 09:11:41 +0530

    client side code for theatres api

--- Book-My-Show Project/client/src/Pages/Profile/index.jsx
+++ Book-My-Show Project/client/src/Pages/Profile/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Tabs} from 'antd';
+import PageTitle from '../../Components/PageTitle';
+import TheatresList from './TheatresList';
 
 const Profile = () => {
   return (
-    <div>Profile Page</div>
+    <div>
+      <PageTitle title="Profile" />
+
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Bookings" key="1">
+           Bookings
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Apply for Theater" key="2">
+           <TheatresList/>
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
   )
 }
 
